63f4efceb358229a05d3c97d8f1fbf17ae985e48
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-04-08 01:42:38 +0200

    feat: prototype

--- src/index.ts
+++ src/index.ts
@@ -4,62 +4,39 @@ import {
   initialize,
   svg2png,
 } from 'svg2png-wasm';
-import marked from 'marked';
 import wasm from './svg2png_wasm_bg.wasm';
 import roboto from './Roboto-Thin.ttf';
+import { svg as svgtemplate } from './template';
 
-const getOptionsFromUrl = (url: string): ConvertOptions => {
+const getOptionsFromUrl = (url: string) => {
   try {
-    const { searchParams } = new URL(url);
-    const scale = Number(searchParams.get('svg2png-scale')) || 1;
-    const backgroundColor = searchParams.get('svg2png-background') || undefined;
-    return { scale, backgroundColor };
-  } catch (e) {
-    return {};
-  }
-};
-
-const getSvgUrl = (source: string): string | undefined => {
-  try {
-    const { href, origin } = new URL(source);
-    const svgPath = new URL(
-      href.substring(origin.length + 1).replace(/(https?:)\/\/?/, '$1//'),
-    );
-    return svgPath.toString();
-  } catch (e) {
-    return undefined;
-  }
-};
-
-const fetchSvg = async (svgUrl: string): Promise<string | Response> => {
-  try {
-    const response = await fetch(svgUrl);
-    const body = await response.text();
-    if (response.ok) return body;
-    return new Response(`SVGFetchError: ${body || response.statusText}`, {
-      status: response.status,
-      statusText: response.statusText,
+    const urlObject = new URL(url);
+    const queryString = urlObject.search.slice(1).split('&');
+    const params: Record<string, string> = {};
+    queryString.forEach((item) => {
+      const kv = item.split('=');
+      if (kv[0]) params[kv[0]] = kv[1];
     });
+    const armor = decodeURI(params.armor).split('%2C') || [];
+    return { armor };
   } catch (e) {
-    return new Response(`${e}`, { status: 500 });
+    return { armor: [] };
   }
 };
 
 const handleRequest = async (req: Request): Promise<Response> => {
   try {
-    const svgUrl = getSvgUrl(req.url);
-    if (svgUrl === undefined) {
-      return new Response(marked(process.env.README ?? ''), {
-        headers: { 'content-type': 'text/html' },
-      });
-    }
-
-    const svg = await fetchSvg(svgUrl);
-    if (svg instanceof Response) return svg;
+    const opt = getOptionsFromUrl(req.url);
+    const svg = svgtemplate
+      .replace('{head}', opt.armor[0])
+      .replace('{shoulders}', opt.armor[1])
+      .replace('{coat}', opt.armor[2])
+      .replace('{gloves}', opt.armor[3])
+      .replace('{leggings}', opt.armor[4])
+      .replace('{shoes}', opt.armor[5]);
 
     await initialize(wasm).catch(() => {});
     const options: ConverterOptions & ConvertOptions = {
-      ...getOptionsFromUrl(req.url),
       fonts: await Promise.all([new Uint8Array(roboto)]),
       defaultFontFamily: {
         sansSerifFamily: 'Roboto',
---
+++ src/template.ts
@@ -0,0 +1,24 @@
+export const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="30.422 43.728 460.362 190" width="460.362" height="190">
+<rect width="200%" height="200%" fill="rgb(47, 49, 54)"/>
+
+<rect x="31.07" y="43.728" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="31.03" y="75.815" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="31.01" y="108.292" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="30.482" y="140.971" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="30.442" y="173.058" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="30.422" y="205.535" width="113.349" height="28.193" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="315.014" y="124.069" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="374.913" y="124.232" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="434.485" y="123.577" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="314.85" y="161.056" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="374.749" y="161.219" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+<rect x="434.321" y="160.564" width="56.299" height="33.059" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
+
+<text x="35.07" y="60">{head}</text>
+<text x="35.07" y="90">{shoulders}</text>
+<text x="35.07" y="120">{coat}</text>
+<text x="35.07" y="155">{gloves}</text>
+<text x="35.07" y="185">{leggings}</text>
+<text x="35.07" y="220">{shoes}</text>
+
+</svg>`;
